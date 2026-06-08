54d469bb4a5fb09f80708be9ab15a9b5485f72e0
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-06-07 21:45:06 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        
+        for(const string& str : strs){
+            encoded += to_string(str.size());
+            encoded += '#';
+            encoded += str;
+        }
+
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded;
+
+        int i = 0;
+
+        while(i < s.size()){
+            string count = "";
+            int j = i;
+
+            while(s[j] != '#'){
+                count += s[j]; 
+                ++j;
+            }
+            int num = stoi(count);
+            string word = s.substr(j + 1, num);
+            decoded.push_back(word);
+            i = j + num;
+            ++i;
+        }
+
+        return decoded;
+    }
+};
