34da6780d12c3c2396eb9fd65aa01315c6b47412
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-07-05 05:51:57 +0000

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,39 @@
+#include <array>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+struct ArrayHash {
+    size_t operator()(const array<int, 26>& arr) const {
+        size_t hash = 0;
+
+        for (int num : arr) {
+            hash = hash * 31 + num;
+        }
+
+        return hash;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<array<int,26>, vector<string>, ArrayHash> mp;
+
+
+        for (const string& s : strs) {
+            array<int,26> count{};
+            for (char c : s) {
+                ++count[c - 'a'];
+            }
+            mp[count].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto& [key, value] : mp) {
+            ans.push_back(move(value));
+        }
+
+        return ans;
+    }
+};
