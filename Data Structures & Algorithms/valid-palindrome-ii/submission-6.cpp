4520be12467cb10c1745947e76ab3eb7368c878a
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-02 02:53:10 -0700

    Add: valid-palindrome-ii - submission-6

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-6.cpp
@@ -0,0 +1,40 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = static_cast<int>(s.size()) - 1;
+
+        while(left < right){
+            
+            if(s[left] != s[right]){
+                return (validPalindrome(s, left + 1, right) || 
+                validPalindrome(s, left, right - 1));
+            }
+
+            else{
+                ++left;
+                --right;
+            }
+        }
+        
+        return true;
+    }
+
+private:
+    bool validPalindrome(const string& s, int left, int right){
+        int l = left;
+        int r = right;
+
+        while(left < right){
+            
+            if(s[left++] != s[right--]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
