5def1bd32e33332f1a60d249700e7d577fee22ff
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-04 14:49:57 -0700

    Add: remove-duplicates-from-sorted-array - submission-7

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-7.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 1;
+        int n = static_cast<int>(nums.size());
+
+        for(int right = 1; right < n; ++right){
+            
+            if(nums[right] != nums[k - 1]){
+                nums[k] = nums[right];
+                ++k;
+            }
+
+        }
+
+        return k;
+    }
+};
