acbafd1906e0f3f4c0e484d35c24eac534a845aa
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-07 00:14:33 -0700

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.cpp
@@ -0,0 +1,22 @@
+#include <utility>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        const int n = static_cast<int>(nums.size());
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, k, n - 1);
+        reverse(nums, 0, k - 1);
+    }
+private:
+    void reverse(vector<int>& nums, int left, int right){
+        while(left < right){
+            swap(nums[left], nums[right]);
+            ++left;
+            --right;
+        }
+    }
+};
