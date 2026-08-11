9cea6b4fd93e4a311e39c4455e98e573c3828c45
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-10 22:19:09 -0700

    Add: lemonade-change - submission-7

---
+++ Data Structures & Algorithms/lemonade-change/submission-7.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0;
+        int ten = 0;
+
+        for(int bill : bills){
+            if(bill == 5){
+                ++five;
+            }
+
+            else if(bill == 10){
+                ++ten;
+                --five;
+            }
+            
+            else{
+                if(ten > 0){
+                    --ten;
+                    --five;
+                }
+
+                else{
+                    five -= 3;
+                }
+            }
+
+            if(five < 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
