073475d1b750a0d520479b01447ed512f8607757
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-11 02:55:03 -0700

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int max_sum = nums[0];
+
+        for(int num : nums){
+            if(sum < 0){
+                sum = 0;
+            }
+            
+            sum += num;
+            max_sum = max(max_sum, sum);
+        }
+
+        return max_sum;
+    }
+};
