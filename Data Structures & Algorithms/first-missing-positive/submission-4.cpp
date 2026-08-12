06b14108e6674f885a03b864e3053367f7d251bb
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-12 00:26:54 -0700

    Add: first-missing-positive - submission-4

---
+++ Data Structures & Algorithms/first-missing-positive/submission-4.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = static_cast<int>(nums.size());
+
+        for(int i = 0; i < n; ++i){
+            
+            while(nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]){
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for(int i = 0; i < n; ++i){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+};
