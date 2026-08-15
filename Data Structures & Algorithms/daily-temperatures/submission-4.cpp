79fd0ad0a032185633db5fa206a2dd3363fd6f52
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-15 11:19:02 -0700

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.cpp
@@ -0,0 +1,24 @@
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        const int n = static_cast<int>(temperatures.size());
+        vector<int> result(n, 0);
+        stack<int> st;
+
+        for(int i = 0; i < n; ++i){
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                result[st.top()] = i - st.top();
+                st.pop();
+            }
+
+            st.push(i);
+        }
+        
+        return result;
+    }
+};
