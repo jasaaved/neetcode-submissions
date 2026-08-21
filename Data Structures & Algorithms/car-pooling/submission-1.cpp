1b60c4ac751b69f918703d750fb0cba35a6bd942
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-20 22:43:55 -0700

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.cpp
@@ -0,0 +1,43 @@
+
+#include <algorithm>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        priority_queue<vector<int>, vector<vector<int>>, Comparer> pq;
+
+        sort(trips.begin(), trips.end(), 
+        [](const vector<int>& a, const vector<int>& b){
+            return a[1] < b[1]; 
+        });
+
+        int passengers = 0;
+        for(const auto& trip : trips){
+            while (!pq.empty() && trip[1] >= pq.top()[2]){
+                passengers -= pq.top()[0];
+                pq.pop();
+            }
+                    
+            passengers += trip[0];
+
+            if(passengers > capacity){
+                return false;
+            }
+            
+            pq.push(trip);
+        }
+
+        return true;
+    }
+
+private:
+    struct Comparer{
+        bool operator()(const vector<int>& a, const vector<int>& b) const {
+            return a[2] > b[2];
+        }
+    };
+};
