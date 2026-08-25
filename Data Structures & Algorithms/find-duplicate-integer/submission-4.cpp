19ab9d27de8faf9b9fb7ed88f7e4fbd9ff54e4e6
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-08-24 22:09:57 -0700

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast = 0;;
+        int slow = 0;
+
+        do {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while(fast != slow);
+
+        slow = 0;
+
+        while(fast != slow){
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+
+        return fast;  
+    }
+};
