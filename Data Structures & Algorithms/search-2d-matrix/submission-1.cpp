51660df5bc7e044d11f26e57e6b4c596fe3f0f04
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-09-01 00:10:22 -0700

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        const int rows = static_cast<int>(matrix.size());
+        const int cols = static_cast<int>(matrix[0].size());
+        int low = 0;
+        int high = rows*cols - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int row = mid / cols;
+            int col = mid % cols;
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+
+            else if(matrix[row][col] < target) {
+                low = mid + 1;
+            }
+
+            else{
+                high = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+};
