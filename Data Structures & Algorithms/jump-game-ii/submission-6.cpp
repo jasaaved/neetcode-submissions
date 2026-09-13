4e3e72772cc3d70fb9aac7d8d5609d0adede1a9d
Jonathan Saavedra <jonathansaavedradev@gmail.com>
2026-09-13 02:14:06 -0700

    Add: jump-game-ii - submission-6

---
+++ Data Structures & Algorithms/jump-game-ii/submission-6.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(const vector<int>& nums) const noexcept {
+        const int n = static_cast<int>(nums.size());
+
+        if (n == 1) {
+            return 0;
+        }
+
+        int current_jump = nums[0];
+        int max_jump = 0;
+        int jump = 1;
+        
+        for (int i = 0; i < n; ++i) {
+            if(current_jump >= n - 1) {
+                return jump;
+            }
+
+            max_jump = max(max_jump, i + nums[i]);
+
+            if (i == current_jump) {
+                current_jump = max_jump;
+                ++jump;
+            } 
+        }
+
+        return jump;
+    }
+};
